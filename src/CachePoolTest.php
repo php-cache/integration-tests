d97f5a48e0f83f68ddeff9e4c9166c7aad255eb2
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-04-03 13:50:07 +0200

    Reduce the sleep time

--- src/CachePoolTest.php
+++ src/CachePoolTest.php
@@ -494,10 +494,10 @@ abstract class CachePoolTest extends \PHPUnit_Framework_TestCase
 
         $item = $this->cache->getItem('key');
         $item->set('value');
-        $item->expiresAfter(2);
+        $item->expiresAfter(1);
         $this->cache->save($item);
 
-        sleep(3);
+        sleep(2);
         $item = $this->cache->getItem('key');
         $this->assertFalse($item->isHit());
         $this->assertNull($item->get(), "Item's value must be null when isHit() is false.");
