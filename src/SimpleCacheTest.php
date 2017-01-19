175302d3058a9b5f3d3331df36c2ccca2dda9436
James Titcumb <james@asgrim.com>
2017-01-19 12:26:29 +0000

    Remove test that would never pass due to PHP casting to integer

--- src/SimpleCacheTest.php
+++ src/SimpleCacheTest.php
@@ -213,11 +213,6 @@ abstract class SimpleCacheTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($result, 'setMultiple() must return true if success');
         $this->assertEquals('value0', $this->cache->get('key0'));
         $this->assertEquals('value1', $this->cache->get('key1'));
-
-//@todo PHP casts '0' to an integer, so this test will never pass
-//        $result = $this->cache->setMultiple(['0' => 'value0']);
-//        $this->assertTrue($result, 'setMultiple() must return true if success');
-//        $this->assertEquals('value0', $this->cache->get('0'));
     }
 
     public function testSetMultipleTtl()
